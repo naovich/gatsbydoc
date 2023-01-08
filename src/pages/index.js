a23e20b74372182047e1f4354e71b7b364e6c76a
nadhoir <attoumani.nadhoir@gmail.com>
2023-01-08 21:29:09 +0100

    fix/deploy_

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations Nadhoir
+        Congratulations Nadhoirovich
         <br />
         <span style={headingAccentStyles}>
           — you just made a Gatsby site! 🎉🎉🎉
